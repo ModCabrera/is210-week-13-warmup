0c90969d3b23fcc8e3dc774d057cab6a50975aa6
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-05-01 01:25:37 +0000

    question

--- boroughs.py
+++ boroughs.py
@@ -22,8 +22,10 @@ def get_score_summary(filename):
         boro = row[1]
         grade = row[10]
         camisid = row[0]
+
         if camisid not in grade_dict and grade in grade_score:
             grade_dict.update({camisid: [grade, boro]})
+
     scorefile.close()
     
     rest_summary = {}
@@ -32,11 +34,9 @@ def get_score_summary(filename):
         grade_letter = grade[0]
         grade_boro = grade[1]
 
-
         if grade_boro in rest_summary:
             rest_summary[grade_boro][1] += grade_score[grade_letter]
             rest_summary[grade_boro][0] += 1
-
         else:
             rest_summary[grade_boro] = [1, grade_score[grade_letter]]
 
@@ -44,20 +44,21 @@ def get_score_summary(filename):
 
     for boro, grade in rest_summary.iteritems():
         final[boro] = grade[0], grade[1]/float(grade[0])
-
     return final
 
 
 import json
+import yaml
 
 
 def get_market_density(filename):
     marketfile = open(filename, 'r')
-    market_read = json.load(marketfile)
+    market_read = yaml.safe_load(marketfile)
     market_data = {}
 
     for values in market_read['data']:
         boro = values[8].strip()
+
         if boro in market_data:
             market_data[boro] += 1
         else:
@@ -66,26 +67,18 @@ def get_market_density(filename):
     return market_data
 
 
-def correlate_data(gradefile, marketfile, outputfile):
-    
-    
-    
-    
+def correlate_data(restaurants, markets, outputfile):
+    restaurants = get_score_summary(restaurants)
+    markets = get_market_density(markets)
+    data_dict = {}
 
+    for key, value in markets.iteritems():
+        boro = key.upper()
+        markval = float(value)
+        restval = float(restaurants[boro][0])
 
+        if boro in restaurants:
+            data_dict[boro] = restaurants[boro][1], markval/restval
 
-
-
-
-
-
-
-
-
-
-
-    
-if __name__ == '__main__':
-    data = get_score_summary('inspection_results.csv')
-    data2 = get_market_density('green_markets.json')
-    print data2
+    with open(outputfile, 'w') as outfile:
+        json.dump(data_dict, outfile)
